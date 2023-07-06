7410c6477e5cad87a24532deb151dcf3b540363c
Maksim Gulay <maksimgulaj@gmail.com>
2023-07-06 10:10:10 +0300

    Added code for using TheCatAPI

---
+++ src/mycat-api.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+const API_KEY = 'live_AYm3OFL5RsopcRxkS6vktyN1rahZysaNXWqUpYbk0YmAblViSMpWRz5uBAhpOooy';
+const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
+axios.defaults.headers.common["x-api-key"] = API_KEY;
+
+
+
+
