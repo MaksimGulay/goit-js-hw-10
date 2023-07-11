53acee2367f7c9b197d8ec3b3bf3cc3455d94c76
Maksim Gulay <maksimgulaj@gmail.com>
2023-07-11 16:01:34 +0300

    fix export function

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,46 +1,18 @@
-const API_KEY = "live_AYm3OFL5RsopcRxkS6vktyN1rahZysaNXWqUpYbk0YmAblViSMpWRz5uBAhpOooy";
+import axios from 'axios';
 
-async function fetchBreeds() {
-  try {
-    const response = await fetch("https://api.thecatapi.com/v1/breeds", {
-      headers: {
-        "x-api-key": API_KEY
-      }
+const API_KEY =
+  'live_AYm3OFL5RsopcRxkS6vktyN1rahZysaNXWqUpYbk0YmAblViSMpWRz5uBAhpOooy';
+
+axios.defaults.headers.common['x-api-key'] = API_KEY;
+
+export function fetchBreeds(BASE_URL) {
+  return axios
+    .get(BASE_URL)
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      console.error('Error fetching breeds:', error);
+      throw error;
     });
-
-    if (!response.ok) {
-      throw new Error("Failed to fetch cat breeds.");
-    }
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    throw new Error("Failed to fetch cat breeds.");
-  }
 }
-
-async function fetchCatsByBreed(breedId) {
-  try {
-    const response = await fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`, {
-      headers: {
-        "x-api-key": API_KEY
-      }
-    });
-
-    if (!response.ok) {
-      throw new Error("Failed to fetch cat information.");
-    }
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    throw new Error("Failed to fetch cat information.");
-  }
-}
-
-async function init() {
-  const breeds = await fetchBreeds();
-  console.log(breeds);
-}
-
-init();
